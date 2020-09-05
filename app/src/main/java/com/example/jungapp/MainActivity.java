67a41c8cdc20a790298bee5ef23bdd17ffdbae0e
TURJOHN <TTURJANN@gmail.com>
2020-09-05 19:05:17 +0200

    Baseline of application
    First commit

--- app/src/main/java/com/example/jungapp/MainActivity.java
+++ app/src/main/java/com/example/jungapp/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.jungapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button openTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        openTest = findViewById(R.id.startButton);
+        openTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startPersonalityTest();
+            }
+        });
+    }
+
+    public void startPersonalityTest(){
+        Intent intent = new Intent(this, PersonalityTestActivity.class);
+        startActivity(intent);
     }
 }
